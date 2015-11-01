2d908df4af9a068f6debd37c7fa6019382ce28d6
Simo Särkkä <simo.sarkka@indooratlas.com>
2015-11-02 00:54:05 +0200

    added

---
+++ kf-benchmark/compile_java.sh
@@ -0,0 +1,1 @@
+javac -classpath /Users/ssarkka/prg/ejml-v0.28-libs/EJML-simple-0.28.jar kf_benchmark.java
---
+++ kf-benchmark/run_java.sh
@@ -0,0 +1,2 @@
+java -classpath ".:/Users/ssarkka/prg/ejml-v0.28-libs/*" kf_benchmark    
+
